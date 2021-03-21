aafc09a314b9b990e38a60dda038525fb1d04e0d
M0wLaue <mewlaue@web.de>
2021-03-22 00:03:05 +0100

    changed from hash to crc algorithm

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import crawler
 import pickle
 import os
+import zlib
 
 sites = set()
 sites.add("https://bytewerk.org")
@@ -10,8 +11,8 @@ sites.add("https://gaestehausleeste.de")
 sites.add("https://k-tronik.de")
 sites.add("https://k-tronik.digital")
 sites.add("https://mantis.zettacloud.de")
-sites.add("https://www.ingolstadt.de")
-
+sites.add("https://toyako.de")
+sites.add("https://wttr.in")
 
 while len(sites) != 0:
     site = sites.pop()
@@ -19,7 +20,7 @@ while len(sites) != 0:
     for website in fetcher.fetch(10):
         for link in website:
             print(link)
-    filename = hash(site)
+    filename = zlib.crc32(bytearray(site, encoding="utf-8"))
     with open(os.path.join("data", str(filename)), "wb") as output_file:
         pickle.dump(fetcher, output_file)
 
--- grapher.py
+++ grapher.py
@@ -1,12 +1,28 @@
 import graphs.igraphCreator
 import os
 import pickle
+import zlib
 
 website_data = None
-with open("fetcher.data", "rb") as input_file:
-    website_data = pickle.load(input_file)
 
-graph = graphs.igraphCreator.NetworkGraph(website_data)
-graph.create()
+sites = set()
+sites.add("https://bytewerk.org")
+sites.add("https://blog.bytewerk.org")
+sites.add("https://stats.bytewerk.org")
+sites.add("https://gaestehausleeste.de")
+sites.add("https://k-tronik.de")
+sites.add("https://k-tronik.digital")
+sites.add("https://mantis.zettacloud.de")
+sites.add("https://toyako.de")
+sites.add("https://wttr.in")
 
-os.startfile("plot.html")
+while len(sites) != 0:
+    site = sites.pop()
+    filename = zlib.crc32(bytearray(site, encoding="utf-8"))
+    with open(os.path.join("data", str(filename)), "rb") as input_file:
+        website_data = pickle.load(input_file)
+
+    graph = graphs.igraphCreator.NetworkGraph(website_data)
+    graph.create()
+
+    os.startfile("plot.html")
